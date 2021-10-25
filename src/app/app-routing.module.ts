48163692db68f9d009f9b72483e5f018dbe0c65a
Veterano <alfaveterano@hotmail.com>
2021-10-25 19:10:57 -0300

    criação de componentes, login, header e navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import {ExemploComponent} from "./exemplo/exemplo.component";
 import {CadastroProdutoComponent} from './cadastro-produto/cadastro-produto.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: "", component: ExemploComponent},
-  { path: "cadastro-produto", component: CadastroProdutoComponent}
+  { path: "cadastro-produto", component: CadastroProdutoComponent},
+  { path: "login", component: LoginComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { ExemploComponent } from './exemplo/exemplo.component';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
+import { LoginComponent } from './components/login/login.component';
+import { CaixaComponent } from './components/caixa/caixa.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExemploComponent,
-    CadastroProdutoComponent
+    CadastroProdutoComponent,
+    LoginComponent,
+    CaixaComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
     imports: [
         BrowserModule,
