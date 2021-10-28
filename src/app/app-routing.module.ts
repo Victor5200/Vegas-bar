7d293ac2998b5ef4f8a58d863ba1cd1565dd0c9b
KlevesonLSilva <kleveson_x3@hotmail.com>
2021-10-28 19:27:21 -0300

    concertando os marges

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
-
-
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ExemploComponent} from './exemplo/exemplo.component';
-import {CadastroProdutoComponent} from './cadastro-produto/cadastro-produto.component';
+
 import {CadastroEstoqueComponent} from './components/cadastro-estoque/cadastro-estoque.component';
-import {CadastroMembrosComponent} from './cadastro-membros/cadastro-membros.component';
+
 import {LoginComponent} from './components/login/login.component';
 import {VedasComandasComponent} from './vedas-comandas/vedas-comandas.component';
-import {ConsultaProdutosComponent} from './consulta-produtos/consulta-produtos.component';
-import {ConsultaMembrosComponent} from './consulta-membros/consulta-membros.component';
-
+import {CadastroProdutoComponent} from './components/cadastro-produto/cadastro-produto.component';
+import {CadastroMembrosComponent} from './components/cadastro-membros/cadastro-membros.component';
+import {ConsultaProdutosComponent} from './components/consulta-produtos/consulta-produtos.component';
+import {ConsultaMembrosComponent} from './components/consulta-membros/consulta-membros.component';
 
 
 const routes: Routes = [
   {path: '', component: ExemploComponent},
   {path: 'cadastro-produto', component: CadastroProdutoComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'cadastro-produto', component: CadastroProdutoComponent},
   {path: 'cadastro-membros', component: CadastroMembrosComponent},
   {path: 'cadastro-estoque', component: CadastroEstoqueComponent},
   {path: 'vendas-comandas', component: VedasComandasComponent},
