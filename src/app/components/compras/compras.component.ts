0188d92f1b9c096deb2100cad6a640960654bf93
KlevesonLSilva <kleveson_x3@hotmail.com>
2021-12-30 14:27:27 -0300

    criando component de compras

---
+++ src/app/components/compras/compras.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {Produto} from '../../shared/models';
+import {ProdutoService} from '../../shared/services/produto-service';
+import {SwallUtil} from '../../shared/util/SwallUtil';
+import {FormControl, FormGroup} from '@angular/forms';
+import {CompraService} from '../../shared/services/compra-service';
+
+@Component({
+  selector: 'app-compras',
+  templateUrl: './compras.component.html',
+  styleUrls: ['./compras.component.scss']
+})
+export class ComprasComponent implements OnInit {
+  retornoApi: Produto[];
+  formProduto: FormGroup;
+
+  constructor(private compraService: CompraService,
+              private produtoService: ProdutoService) {
+  }
+
+  ngOnInit(): void {
+    this.buscar();
+    this.createForm();
+
+  }
+
+  createForm(): void {
+    this.formProduto = new FormGroup({
+      quantidadeProduto: new FormControl(null),
+      produto: new FormControl(null)
+    });
+
+  }
+
+  buscar(): void {
+    this.produtoService.buscarTodas().subscribe(resultado => {
+      this.retornoApi = resultado;
+    });
+  }
+
+  salvar(): void {
+    const compra = this.formProduto.value;
+    debugger
+    this.compraService.salvar(compra).subscribe(() => SwallUtil.mensagemSucesso('Deu Certo!!!'),
+      error => SwallUtil.mensagemError('Deu errado besta!'));
+
+  }
+}
---
+++ src/app/shared/services/compra-service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Membro, Produto} from "../models";
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ProdutoService {
+
+  api = `${environment.baseUrl}/produtos`
+
+  constructor(protected http: HttpClient) {
+  }
+
+  salvar(obj: any) {
+    return this.http.post<Produto>(this.api, obj)
+  }
+
+  buscarTodas(){
+    return this.http.get<Produto[]>(this.api);
+  }
+
+  buscarPorId(id){
+    return this.http.get<Produto>(this.api + '/' + id);
+  }
+
+  deletar(id) {
+    return this.http.delete(this.api + '/' + id);
+  }
+}
