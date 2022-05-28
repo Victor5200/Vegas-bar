091bfefad9a9d3739dce9ea018637dc6e6bf85c6
Victor Alex <victoralexsander5200@gmail.com>
2022-05-27 21:22:39 -0300

    ajustando layout mobile e expiracao jwt

--- src/app/interceptor/jwt.interceptor.ts
+++ src/app/interceptor/jwt.interceptor.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {AuthenticationService} from "../shared/services/authentication.service";
 import {catchError, map} from "rxjs/operators";
 import {NgxSpinnerService} from "ngx-spinner";
+import {Router} from "@angular/router";
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService,
+              private router: Router) { }
 
   intercept(
     req: HttpRequest<any>,
@@ -26,10 +28,25 @@ export class JwtInterceptor implements HttpInterceptor {
 
     return next.handle(req)
       .pipe(catchError((err) => {
-        return err;
+        if (err.status === 401) {
+          const currentUser = this.authenticationService.currentUserValue;
+
+          if (this.isTokenExpired(currentUser)) {
+            this.authenticationService.logout();
+            this.router.navigate(['/login'], { queryParams: { returnUrl: '/consulta-venda' } });
+          }
+        }
+
+        const error = err.error.message || err.statusText;
+        return throwError(error);
       }))
       .pipe(map<HttpEvent<any>, any>((evt: HttpEvent<any>) => {
         return evt;
       }));
+  }
+
+  isTokenExpired(token) {
+    const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
+    return (Math.floor((new Date).getTime() / 1000)) >= expiry;
   }
 }
